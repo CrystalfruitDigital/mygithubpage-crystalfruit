2b2788aa62b191bc170c9b487de40bc735bd76e6
John Nguyen <tehjawn@gmail.com>
2015-08-01 00:38:12 -0400

    #BOOST one-page coming soon v3

--- p/hashtagboost/one-page/assets/main.js
+++ p/hashtagboost/one-page/assets/main.js
@@ -9,7 +9,7 @@ $(document).ready(function()
 
     if( $('#more-content').hasClass('shown') )
     {
-      $('#load-more-content').text('Hide content');
+      $('#load-more-content').text('< Back');
       $('#more-content').fadeIn('slow', function()
       {
         $('#load-more-content').fadeIn('slow');
@@ -17,7 +17,7 @@ $(document).ready(function()
     }
     else
     {
-      $('#load-more-content').text('Load some content');
+      $('#load-more-content').text('Learn More');
       $('#more-content').fadeOut('slow', function()
       {
         $('#load-more-content').fadeIn('slow');
