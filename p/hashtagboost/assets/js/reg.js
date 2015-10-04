d04873b92e32435f8a52f2e000dec5a53756afa5
John Nguyen <tehjawn@gmail.com>
2015-10-04 14:44:40 -0400

    #BOOST Register Page v0.1
    - Not encrypted
    - No info feedback upon success/failure
    - Email checker not implemented
    - Instagram OAUTH not implemented

--- p/hashtagboost/assets/js/reg.js
+++ p/hashtagboost/assets/js/reg.js
@@ -1,9 +1,47 @@
-toggleTerms = function(){
-	$("#termsWindow").slideToggle('fast');
+toggleTerms = function() {
+    $("#termsWindow").slideToggle('slow');
 }
 
 var app = angular.module("registerUser", []);
 
-app.controller("mainCtrl", function($scope){
-	$scope.user;
+app.controller("mainCtrl", function($scope) {
+
+    registerNewUser = function() {
+        if ($scope.password == $scope.vPass) {
+            var newuser = {
+                'firstname': $scope.firstName,
+                'lastname': $scope.lastName,
+                'email': $scope.email,
+                'password': $scope.password,
+                'type': 1,
+                'instagram': $scope.instagram,
+                'audience': $scope.audience,
+                'terms': $scope.termsChecked
+            }
+
+            $.ajax({
+                url: "http://ec2-54-172-0-233.compute-1.amazonaws.com/addnewuser",
+                datatype: 'json',
+                type: 'post',
+                data: {
+                    'user': newuser
+                },
+                success: function(data) {
+                    if (data.status == '409') {
+                        alert('E-mail already in use!');
+                    } else if (data.status == 'success') {
+                        alert('successfully added to db');
+                        $("#thankyou").slideToggle('fast');
+                    }
+                }.bind(this),
+                error: function(xhr, status, err) {
+                    console.error(xhr, status, err.tostring());
+                }.bind(this)
+            });
+        } else {
+            alert("Passwords do not match!");
+            $scope.vPass = "";
+        }
+    }
+
 });
